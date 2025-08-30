adb97f6135476bcf745b9c4514937e79b44e5e25
Aishwary Gupta <74300436+011aishwary@users.noreply.github.com>
2025-08-31 04:29:42 +0530

    Update example_usage.py

--- CNN Model/example_usage.py
+++ CNN Model/example_usage.py
@@ -9,7 +9,7 @@ model = tf.keras.models.load_model('mangrove_deforestation_model.keras')
 print("Model loaded successfully!")
 
 # 2. Preprocess a new image (they would replace this path)
-img_array = load_and_preprocess_image('path_to_your_test_image.jpg')
+img_array = load_and_preprocess_image('https://www.shutterstock.com/shutterstock/photos/1659051325/display_1500/stock-photo-log-trunks-pile-the-logging-timber-forest-wood-industry-wide-banner-or-panorama-heavy-wood-trunks-1659051325.jpg')
 
 # 3. Make a prediction
 class_name, confidence = predict_class(model, img_array)
